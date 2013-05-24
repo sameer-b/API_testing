4cf28429a4907f390ff2ab0cf2295abd425970d4
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-05-24 17:40:12 +0530

    code cleaning

--- API_Testing_JS.js
+++ API_Testing_JS.js
@@ -1,19 +1,22 @@
+//get the button and attach the event. Because inline event handlers aren't cool.
 var button=document.getElementById('makeRequestButton');
 button.addEventListener('click',callApi,false);
 function callApi()
-{
+{  	//get the values from the input
 	var url=document.getElementById('url_Input').value;
     var method=document.getElementById('method').value;
+    //create XMLHttpRequest object
     var xmlhttp=new XMLHttpRequest();
 
-
+    //display information when the state changes
 	xmlhttp.onreadystatechange = function() {
 	 if (xmlhttp.readyState === 4 )
+	 	//get the response and inject it into the html
     	var response=xmlhttp.responseText;
   		var display=document.getElementById('displayResult');
   		display.innerHTML= response;
 } 
-
+   //alas! open and send!
 	xmlhttp.open(method,url,true);
 	xmlhttp.send();
 }
