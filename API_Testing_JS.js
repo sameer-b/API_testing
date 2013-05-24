9c1d3a660f6ebea6f4494d11453b7c5e3cf6838f
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-05-24 17:36:16 +0530

    improved the output screen

--- API_Testing_JS.js
+++ API_Testing_JS.js
@@ -1,37 +1,21 @@
-
-
-
 var button=document.getElementById('makeRequestButton');
 button.addEventListener('click',callApi,false);
-
-
-
 function callApi()
 {
 	var url=document.getElementById('url_Input').value;
     var method=document.getElementById('method').value;
-	//alert(url);
-	//alert(method);
-var xmlhttp=new XMLHttpRequest();
+    var xmlhttp=new XMLHttpRequest();
 
 
-xmlhttp.onreadystatechange = function() {
+	xmlhttp.onreadystatechange = function() {
 	 if (xmlhttp.readyState === 4 )
-    var response=xmlhttp.responseText;
-  var display=document.getElementById('displayResult');
-  display.innerHTML= response;
-  //  alert(response);
-	
-	} 
+    	var response=xmlhttp.responseText;
+  		var display=document.getElementById('displayResult');
+  		display.innerHTML= response;
+} 
 
-xmlhttp.open("GET","https://api.github.com/legacy/user/search/:user",true);
-xmlhttp.send();
-
-	
-   
-
-
-
+	xmlhttp.open(method,url,true);
+	xmlhttp.send();
 }
 
 
